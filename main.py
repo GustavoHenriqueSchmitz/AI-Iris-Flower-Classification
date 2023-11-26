8871aedc764ee2e568c8592196b79049f93d7c4a
GustavoHenriqueSchmitz <gustavohenriqueschmitz568@gmail.com>
2023-11-26 14:23:13 -0300

    finishing

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ train_data = pd.read_csv('./IRIS_Train.csv')
 test_data = pd.read_csv('./IRIS_Test.csv')
 
 # Preprocessing the data for train
-X_train = train_data.drop('species', axis=1) # Separate the necessary data for the AI analyse and classify
+x_train = train_data.drop('species', axis=1) # Separate the necessary data for the AI analyse and classify
 y_train = pd.get_dummies(train_data.species) # Convert categorical variable into dummy/indicator variables
 
 # Preprocessing the data for test
-X_test = test_data.drop('species', axis=1) # Separate the necessary data for the AI analyse and classify
+x_test = test_data.drop('species', axis=1) # Separate the necessary data for the AI analyse and classify
 y_test = pd.get_dummies(test_data.species) # Convert categorical variable into dummy/indicator variables
 
 # Create the model
 model = keras.Sequential([
-    keras.layers.Dense(64, activation='relu', input_shape=[len(X_train.keys())]),
+    keras.layers.Dense(64, activation='relu', input_shape=[len(x_train.keys())]),
     keras.layers.Dense(64, activation='relu'),
     keras.layers.Dense(3, activation='softmax')
 ])
@@ -27,6 +27,18 @@ model.compile(
     metrics=['accuracy']
 )
 
-history = model.fit(X_train, y_train, epochs=1000, validation_split = 0.2)
+history = model.fit(x_train, y_train, epochs=300, validation_split = 0.2)
+test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
 
-test_loss, test_acc = model.evaluate(X_test, y_test, verbose=2)
+print("================= Test Results ==================")
+print(f"Loss: {test_loss} | Accurate: {test_acc}")
+print("=================================================")
+
+predictions = model.predict(pd.DataFrame([[5.3, 3.7, 1.5, 0.2]]))
+for counter, prediction in enumerate(predictions[0]):
+    if counter == 0:
+        print(f"Iris-setosa: {prediction * 100}")
+    elif counter == 1:
+        print(f"Iris-versicolor: {prediction * 100}")
+    else:
+        print(f"Iris-virginica: {prediction * 100}")
