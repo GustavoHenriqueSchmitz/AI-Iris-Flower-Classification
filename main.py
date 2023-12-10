99895f0e66e6657ed7b4126fd082db70eebba131
GustavoHenriqueSchmitz <gustavohenriqueschmitz568@gmail.com>
2023-12-10 16:04:33 -0300

    save-model-functionality

--- main.py
+++ main.py
@@ -1,50 +1,21 @@
-import tensorflow as tf
 from tensorflow import keras
 import pandas as pd
 
-# Getting the data
-train_data = pd.read_csv('./IRIS_Train.csv')
-test_data = pd.read_csv('./IRIS_Test.csv')
+# Load the model for use
+loaded_model = keras.models.load_model('IRIS_Classification_model')
 
-# Preprocessing the data for train
-x_train = train_data.drop('species', axis=1) # Separate the necessary data for the AI analyse and classify
-y_train = pd.get_dummies(train_data.species) # Convert categorical variable into dummy/indicator variables
-
-# Preprocessing the data for test
-x_test = test_data.drop('species', axis=1) # Separate the necessary data for the AI analyse and classify
-y_test = pd.get_dummies(test_data.species) # Convert categorical variable into dummy/indicator variables
-
-# Create the model
-model = keras.Sequential([
-    keras.layers.Dense(64, activation='relu', input_shape=[len(x_train.keys())]),
-    keras.layers.Dense(64, activation='relu'),
-    keras.layers.Dense(3, activation='softmax')
-])
-
-# Compile the model
-model.compile(
-    loss='categorical_crossentropy', # Loss function
-    optimizer='adam', # Optimizer
-    metrics=['accuracy'] # Metric to monitor
-)
-
-# Train the model
-history = model.fit(x_train, y_train, epochs=300, validation_split = 0.2)
-
-# Evaluate the model on the test data
-test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
-
-# Print the test results
-print("================= Test Results ==================")
-print(f"Loss: {test_loss} | Accurate: {test_acc}")
-print("=================================================")
+print("======================================================")
+flower_size_values = []
+for size_type in ["sepal_length", "sepal_width", "petal_length", "petal_width"]:
+    flower_size_values.append(float(input(f"{size_type}: ")))
 
 # Make a prediction on a new data point
-predictions = model.predict(pd.DataFrame([[5.3, 3.7, 1.5, 0.2]]))
+predictions = loaded_model.predict(pd.DataFrame([flower_size_values]))
 for counter, prediction in enumerate(predictions[0]):
     if counter == 0:
-        print(f"Iris-setosa: {prediction * 100}")
+        print(f"Iris-setosa: {prediction * 100}%")
     elif counter == 1:
-        print(f"Iris-versicolor: {prediction * 100}")
+        print(f"Iris-versicolor: {prediction * 100}%")
     else:
-        print(f"Iris-virginica: {prediction * 100}")
+        print(f"Iris-virginica: {prediction * 100}%")
+            
---
+++ train.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+from tensorflow import keras
+import pandas as pd
+
+# Getting the data
+train_data = pd.read_csv('./IRIS_Train.csv')
+test_data = pd.read_csv('./IRIS_Test.csv')
+
+# Preprocessing the data for train
+x_train = train_data.drop('species', axis=1) # Separate the necessary data for the AI analyse and classify
+y_train = pd.get_dummies(train_data.species) # Convert categorical variable into dummy/indicator variables
+
+# Preprocessing the data for test
+x_test = test_data.drop('species', axis=1) # Separate the necessary data for the AI analyse and classify
+y_test = pd.get_dummies(test_data.species) # Convert categorical variable into dummy/indicator variables
+
+# Create the model
+model = keras.Sequential([
+    keras.layers.Dense(64, activation='relu', input_shape=[len(x_train.keys())]),
+    keras.layers.Dense(64, activation='relu'),
+    keras.layers.Dense(3, activation='softmax')
+])
+
+# Compile the model
+model.compile(
+    loss='categorical_crossentropy', # Loss function
+    optimizer='adam', # Optimizer
+    metrics=['accuracy'] # Metric to monitor
+)
+
+# Train the model
+history = model.fit(x_train, y_train, epochs=300, validation_split = 0.2)
+
+# Evaluate the model on the test data
+test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+
+# Print the test results
+print("================= Test Results ==================")
+print(f"Loss: {test_loss} | Accurate: {test_acc}")
+print("=================================================")
+
+while True:
+    answer = str(input("According the results, do you want to continue and save the model for use [y/n]: ")).lower()
+    if answer != "n" and answer != "y":
+        print("Digit a valid answer please.")
+        continue
+    else:
+        print("Saving Model...")
+        # Save the model
+        model.save('IRIS_Classification_model')
+        print("Model saved.")
+        break
+
+# # Make a prediction on a new data point
+# predictions = model.predict(pd.DataFrame([[5.3, 3.7, 1.5, 0.2]]))
+# for counter, prediction in enumerate(predictions[0]):
+#     if counter == 0:
+#         print(f"Iris-setosa: {prediction * 100}")
+#     elif counter == 1:
+#         print(f"Iris-versicolor: {prediction * 100}")
+#     else:
+#         print(f"Iris-virginica: {prediction * 100}")
